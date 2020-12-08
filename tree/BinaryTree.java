3d7ec8de56c434d3e24588eb97d8983a7d87c4cf
Zhongyang MA <mzy@mail.ustc.edu.cn>
2020-12-08 19:09:33 +0800

    adddd

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -131,8 +131,21 @@ public class BinaryTree {
     // 倒序层次遍历
 
     // 二叉树深度
+    public static int depth(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+        return (left > right ? left : right) + 1;
+    }
 
     // 二叉树节点数
+    public static int countNode(TreeNode root) {
+        if(root == null) return 0;
+        int left = countNode(root.left);
+        int right = countNode(root.right);
+        return 1 + left + right;
+    }
 
     public static void main(String[] args) {
         TreeNode tree = createTree();
@@ -148,5 +161,10 @@ public class BinaryTree {
         // 锯齿形层次遍历
         System.out.println("锯齿形层次遍历");
         zigzagLevelOrder(tree);
+        // 二叉树深度
+        System.out.println("二叉树深度:" + depth(tree));
+        // 二叉树节点数
+        System.out.println("二叉树节点数:" + countNode(tree));
+
     }
 }
