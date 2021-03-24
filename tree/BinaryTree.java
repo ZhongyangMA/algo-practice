cc6d180e2f10ce3e730d56a53b49b39717e58619
Zhongyang MA <zhongyang.ma@ximalaya.com>
2021-03-24 16:18:17 +0800

    AVL平衡二叉树

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -203,6 +203,17 @@ public class BinaryTree {
         return (left > right ? left : right) + 1;
     }
 
+    // 判断平衡二叉树
+    public static boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        if(Math.abs(depth(root.left) - depth(root.right)) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
     // 二叉树节点数
     public static int countNode(TreeNode root) {
         if(root == null) return 0;
